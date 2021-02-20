2ca6c675b02254c1c5e1743b228b6ab6d48fb5c4
Mias <812137533@qq.com>
2021-02-20 14:19:56 +0800

    update

--- scripts/bio-init.js
+++ scripts/bio-init.js
@@ -1,24 +1,33 @@
 const fs = require('fs-extra')
 const path = require('path')
-const postsPath = path.join(__dirname, '../pages/posts')
-const aboveMePath = path.join(__dirname, '../pages/aboveMe')
-const dataPath = path.join(__dirname, '../lib/data/metaData.json')
+const pagePath = path.resolve(__dirname, '../pages')
+const pageFileRoot = path.resolve(__dirname, '../pages')
+const dataPath = path.resolve(__dirname, '../lib/data')
 const chalk = require('chalk')
 
-const delteDirInBio = async (files, parent_filePath) => {
-  return Promise.all(
+const delDirOrFile = async (files, filePath) => {
+  try {
     files.map(async (file) => {
-      const full_path_name = path.join(parent_filePath, file)
-      await fs.remove(full_path_name)
+      const _splitPath = path.join(filePath, file)
+      await fs.remove(_splitPath)
     })
-  )
+  } catch (error) {
+    console.log(error)
+  }
 }
+
 !(async () => {
-  const postFiles = await fs.readdir(postsPath)
-  const aboveMeFiles = await fs.readdir(aboveMePath)
-  const dataJson = await fs.readdir(dataPath)
-  await delteDirInBio(postFiles, postsPath)
-  await delteDirInBio(aboveMeFiles, aboveMePath)
-  await delteDirInBio(dataJson, dataPath)
-  console.log(chalk.hex('#FF1B68')('init success~ have Enjoy it'))
+  try {
+    const pages = await fs.readdir(pagePath)
+    const dataFile = pages.filter((file) => !file.endsWith('.jsx'))
+    const dataBase = await fs.existsSync(dataPath)
+    if (dataBase) return fs.remove(dataPath)
+    if (dataFile.length !== 0) {
+      delDirOrFile(dataFile, pageFileRoot)
+    }
+    console.log(chalk.hex('#FF1B68')('init the bio success~ have Enjoy it'))
+  } catch (error) {
+    console.log(error)
+    process.exit(0)
+  }
 })()
